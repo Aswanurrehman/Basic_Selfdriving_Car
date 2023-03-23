6198444db9aeccaa8b99c2c607e25e26d89ab7cc
Aswan Ur Rehman <aswanurrehman@gmail.com>
2023-03-23 12:36:25 +0530

    This commit contains track1.py file

---
+++ track1.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+window = pygame.display.set_mode((1200, 400))
+track = pygame.image.load('track1.png')
+car = pygame.image.load('tesla.png')
+car = pygame.transform.scale(car, (30, 60))
+car_x = 155
+car_y = 300
+while True:
+    window.blit(track, (0, 0))
+    window.blit(car, (car_x, car_y))
+    pygame.display.update()
