e74e3ede3ba5c004bd52b4649f6683cc0cac50bb
Aswan Ur Rehman <aswanurrehman@gmail.com>
2023-03-23 13:01:07 +0530

    This is final commit

--- track6.py
+++ track6.py
@@ -6,7 +6,10 @@ car = pygame.image.load('tesla.png')
 car = pygame.transform.scale(car, (30, 60))
 car_x = 155
 car_y = 280
+clock = pygame.time.Clock()
 while True:
+    clock.tick(60)
+    car_y = car_y-2
     window.blit(track, (0, 0))
     window.blit(car, (car_x, car_y))
     pygame.display.update()
